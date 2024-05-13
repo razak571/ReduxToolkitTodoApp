78957b267b5b38e5fbf4f1010dcb8a7e256058c7
razak attar <razakattar0221@gmail.com>
2024-05-14 01:53:32 +0530

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Todos from "./components/Todos";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Redux Toolkit</h1>
+      <h1 className="text-3xl font-bold underline">Redux Tool Kit</h1>
       <AddTodo />
       <Todos />
     </>
